1ba8cb2d08900ccc9dddebb57518e686fceed274
Max Malinovsky <gudzik@yahoo.com>
2023-01-22 12:41:32 +0200

    Project with connetions, but with bug 11/11

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -16,17 +16,21 @@ final class MovieQuizViewController: UIViewController {
     QuizQuestion(image: "", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
     ]
     private var currentQuestionIndex = 0
+    private var correctAnswers = 0
     
     // MARK: - Outlets
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
     
+    // MARK: - Alerts
+    let action = UIAlertAction(title: "Сыграть ещё раз", style: .default, handler: { _ in
+    })
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
       super.viewDidLoad()
-        
-        
+      showNextQuestionsOrResults()
     }
     
     // MARK: - Actions
@@ -43,11 +47,26 @@ final class MovieQuizViewController: UIViewController {
     }
     // MARK: - Functions
     private func show(quiz step: QuizStepViewModel){
-        
+        imageView.image = step.image
+        textLabel.text = step.question
+        counterLabel.text = step.questionNumber
     }
     
     private func show(quiz result: QuizResultsViewModel) {
-        
+        let alert = UIAlertController(title: result.title, // заголовок всплывающего окна
+                                      message: result.text, // текст во всплывающем окне
+                                      preferredStyle: .alert) // preferredStyle может быть .alert или .actionSheet
+
+        let action = UIAlertAction(title: result.buttonText, style: .default, handler: { _ in
+          // вот этот код с переключением индекса и показом первого вопроса надо будет написать тут
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        })
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel{
@@ -56,17 +75,33 @@ final class MovieQuizViewController: UIViewController {
     
     private func showAnswerResult(isCorrect: Bool){
         imageView.layer.masksToBounds = true
-        imageView.layer.borderWidth = 1
-        imageView.layer.borderColor = UIColor.white.cgColor
-        imageView.layer.cornerRadius = 6
-        print(isCorrect)
+        imageView.layer.borderWidth = 8
+        imageView.layer.cornerRadius = 20
         if isCorrect{
-            imageView.layer.borderColor = UIColor.green.cgColor
+            correctAnswers += 1
+            imageView.layer.borderColor = UIColor.ypGreen.cgColor
         }else{
-            imageView.layer.borderColor = UIColor.red.cgColor
+            imageView.layer.borderColor = UIColor.ypRed.cgColor
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0){
+            self.showNextQuestionsOrResults()
         }
     }
     
+    private func showNextQuestionsOrResults(){
+        if currentQuestionIndex == questions.count - 1{
+            // показать результат квиза
+            let text = "Ваш результат :\(correctAnswers)/\(questions.count)"
+            let viewModel = QuizResultsViewModel(title: "Этот раунд окончен", text: text, buttonText: "Сыграть еще раз")
+        }else{
+            currentQuestionIndex += 1
+            // показать следующий вопрос (через 1 секунду)
+            let question = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: question)
+            self.show(quiz: viewModel)
+            // Вот здесь нужно дописать функцию, считывающую инфу с вьюмодели и раскидывающую по вьюшкам
+        }
+    }
 }
 
 // MARK: - Additional data structures
