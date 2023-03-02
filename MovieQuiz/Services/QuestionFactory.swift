c7118ba98b1a4d6402ee62513d28aeae5aa4ecdd
Max Malinovsky <gudzik@yahoo.com>
2023-03-02 15:47:24 +0200

    Fixed literal

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -39,7 +39,7 @@ class QuestionFactory: QuestionFactoryProtocol{
             
             let rating = Float(movie.rating) ?? 0
             
-            let text = "Рейтинг этого фильма больше, чем 7"
+            let text = "Рейтинг этого фильма больше, чем 7?"
             let correctAnswer = rating > 7
             let question = QuizQuestion(image: imageData,
                                         text: text,
