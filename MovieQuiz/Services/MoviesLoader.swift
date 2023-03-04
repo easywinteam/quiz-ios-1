2aa661f12c790b8026b67ba52f982b8488737527
Max Malinovsky <gudzik@yahoo.com>
2023-03-04 14:25:28 +0200

    Added unit test

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -6,7 +6,11 @@ protocol MoviesLoading{
 
 struct MoviesLoader: MoviesLoading{
     //MARK: - Network Client
-    private let networkClient = NetworkClient()
+    private let networkClient: NetworkRouting
+    
+    init(networkClient: NetworkRouting = NetworkClient()){
+        self.networkClient = networkClient
+    }
     
     //MARK: - URL
     private var mostPopularMoviesURL: URL{
